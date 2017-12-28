1e35c4f154dded6ab05fad7af11c048058754b75
Natalia Vetter <nataliavetter@gmail.com>
2017-12-28 21:45:12 +0100

    Update dependencies and remove unused code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="c-App">
+        <header className="c-App__header">
+          <h1 className="c-App__title">Online bank account application</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
